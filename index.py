9e2836638f1d80d79bc84cd6c0e5a5fc6007a790
kadir-zeren <k4dzeren@gmail.com>
2024-02-17 19:22:57 +0300

    spe end

--- index.py
+++ index.py
@@ -1,1 +1,96 @@
 print('Hello')
+
+print(3+5)
+print('3'+'5')
+print(3*5)
+print('abc'*3)
+
+print(11-7)
+print(4-11.0)
+print('11 - 7')
+
+print('clarusway')
+print('3+5')
+print('abc')
+
+num1,num2=5,3
+num3=num1-num2
+print(num3)
+
+r = 5
+pi = 3.14
+area = pi * (r ** 2)
+print(area)
+
+print(11%2)
+print((4*5)/2)
+
+print(2**3)
+print(3**2)
+a = 2
+b = 8
+print((a*b)**0.5)
+
+a=5
+a= a+1
+ 
+a+= 1
+print(a)
+
+A = 10
+A= A *2
+A*= 2
+
+print(A)
+
+print(3+5*2)
+
+a = -3 ** 2
+print(a)
+
+a = (-3) ** 2
+print(a)
+
+a = 3
+b = 4
+hipotenus = (a**2 + b**2)**0.5
+print(hipotenus)
+
+print(a,b,hipotenus)
+print(a,b,hipotenus,sep='------->')
+
+print(a)
+print(b)
+print(hipotenus)
+
+
+print(a,end='bitiiiii')
+print(b)
+print(hipotenus)
+
+bir = 'ilk satir'
+iki = 'ikinci satir'
+uc = 'ucuncu satir'
+
+print(bir,iki,uc)
+print(bir,iki,uc,sep='____bosluk____')
+
+print(bir)
+print(iki)
+print(uc)
+
+print(bir,end='\n')
+print(iki,end='\n')
+print(uc)
+
+print(bir,end='\n*****')
+print(iki,end='\n')
+print(uc)
+
+print(bir,iki,uc,sep='* - *',end='&')
+print(bir,iki,uc,sep='')
+print(bir,iki,uc,sep='')
+
+print(bir,iki,uc,sep='* - *',end='&\n')
+print(bir,iki,uc,sep='')
+print(bir,iki,uc,sep='')
